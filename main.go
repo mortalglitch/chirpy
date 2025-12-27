c3816b9ab11831d2b888b4f1db30270216e6031a
mortalglitch <mortal.glitch@gmail.com>
2025-12-27 10:36:51 -0500

    Added SQL queries and schema for users. Updated main to iniate connection with DB and read env.

--- main.go
+++ main.go
@@ -1,21 +1,41 @@
 package main
 
+import _ "github.com/lib/pq"
+
 import (
+	"database/sql"
 	"log"
 	"net/http"
+	"os"
 	"sync/atomic"
+
+	"github.com/joho/godotenv"
+
+	"github.com/mortalglitch/chirpy/internal/database"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	db             *database.Queries
 }
 
 func main() {
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil{
+		log.Fatalf("error connecting to db: %v", err)
+	} 
+
 	const filepathRoot = "."
 	const port = "8080"
 
+	dbQueries := database.New(db)
+
 	apiCfg := apiConfig{
 		fileserverHits: atomic.Int32{},
+		db:             dbQueries,
 	}
 
 	mux := http.NewServeMux()
